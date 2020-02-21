f8c44e204b2fbffc602bc055aa5129b68104cbf8
Timo Klüber <timo.klueber@baloise.ch>
2020-02-21 14:50:33 +0100

    initial commit

---
+++ src/main/java/ch/baloise/klueber/reservationdemo/ReservationDemoApplication.java
@@ -0,0 +1,13 @@
+package ch.baloise.klueber.reservationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/baloise/klueber/reservationdemo/ReservationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.baloise.klueber.reservationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
